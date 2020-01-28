53b452a6accd079f63e611df29eeb06f8f69d280
jsraoshihong@163.com <jsraoshihong@163.com>
2020-01-28 17:53:37 +0800

    commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,46 @@
 module.exports = {
-    base: '/blog-demo/',
-    title: 'blog-demo',
+    base: '/blog-demo/',//项目根目录
+    title: 'D之Y族',//标题
     description: 'Vuepress blog demo',
     themeConfig: {
         repo: 'https://github.com/raoshihong',
         repoLabel: 'My GitHub',
-        nav: [
+        nav: [//导航栏
             {text: 'Home',link: '/'},
-            {text: "FistBlog", link: '/blog/FirstBlog.md'}
+            {text: "FistBlog", link: '/blog/java/FirstBlog.md'}
         ],
-        sidebar: [
-            ['/', '首页'],
-            ['/blog/FirstBlog.md', '我的第一篇博客']
+        sidebar: [//侧栏
+            {
+                title: '首页',
+                path: '/'
+            },
+            {
+                title: 'java',
+                children: [
+                    {
+                        title: 'juc',
+                        path: '/blog/java/FirstBlog.md'
+                    },
+                    {
+                        title: 'collections',
+                        path: '/blog/java/SecondBlog.md'
+                    }
+                ]
+            },
+            {
+                title: 'java',
+                children: [
+                    {
+                        title: 'spring',
+                        path: '/blog/spring/FirstBlog.md'
+                    },
+                    {
+                        title: 'collections',
+                        path: '/blog/spring/SecondBlog.md'
+                    }
+                ]
+            }
+            
           ]
     }
 }
